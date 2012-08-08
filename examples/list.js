1984dfa64e662230cb7f20a9a604c62e12a66345
Gabriel Llamas <gabriel_llamas_llopis@yahoo.es>
2012-08-08 10:56:10 +0200

    updated example

--- examples/list.js
+++ examples/list.js
@@ -117,7 +117,7 @@ create (function (){
 					}
 					*/
 					
-					//Deep 3 with a synchronous/asynchronous filter, therefore only the of
+					//Deep 3 with a synchronous/asynchronous filter, therefore only the content of
 					//"a", "b", "c", "d" and "e" will be showed
 					//If we found a file named a1.txt we'll calculate its MD5 checksum and then
 					//continue the listing
